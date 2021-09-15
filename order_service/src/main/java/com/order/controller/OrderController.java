b97e049061f9a98bfd47be997854daa21f1d1065
apple-notebook <575403776@qq.com>
2021-09-15 23:44:53 +0800

    eureka和ribbon

--- order_service/src/main/java/com/order/controller/OrderController.java
+++ order_service/src/main/java/com/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.order.controller;
 
 import com.order.entity.ProductInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +17,29 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Value("${spring.cloud.client.ip-address}")
+    private String ip;
+
+    @Value("${server.port}")
+    private String port;
+
     @Resource
     private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String queryProductInfoById(@PathVariable Integer id) {
-        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
-        ServiceInstance instance = instances.get(0);
-        ProductInfo productInfo = restTemplate.getForObject("http://" + instance.getHost() + ":" + instance.getPort() + "/productCtrl/" + id, ProductInfo.class);
-        return productInfo.toString();
+    public ProductInfo queryProductInfoById(@PathVariable Integer id) {
+        ProductInfo productInfo = restTemplate.getForObject("http://" +"service-product"+"/productCtrl/" + id, ProductInfo.class);
+
+        return productInfo;
     }
+
+//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//    public String queryProductInfoById(@PathVariable Integer id) {
+//        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+//        ServiceInstance instance = instances.get(0);
+//        ProductInfo productInfo = restTemplate.getForObject("http://" + instance.getHost() + ":" + instance.getPort() + "/productCtrl/" + id, ProductInfo.class);
+//        return productInfo.toString();
+//    }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String addProduct(@RequestBody ProductInfo productInfo) {
--- product_service/src/main/java/com/product/controller/ProductController.java
+++ product_service/src/main/java/com/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.product.controller;
 
 import com.product.entity.ProductInfo;
 import com.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,9 +14,16 @@ public class ProductController {
     @Resource
     private ProductService productService;
 
+    @Value("${spring.cloud.client.ip-address}")
+    private String ip;
+
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public ProductInfo queryProductInfoById(@PathVariable Integer id){
         ProductInfo productInfo = productService.queryProductById(id);
+        productInfo.setProductName("IP地址："+ip+":"+port);
         return productInfo;
     }
 
